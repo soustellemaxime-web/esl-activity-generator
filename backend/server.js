f8d86f32a62b45d4aedfbb35038f2353eecd3725
Maxime Soustelle <soustelle.maxime@gmail.com>
2026-03-18 11:59:03 +0700

    bingo pdf creation

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 const express = require("express")
 const cors = require("cors")
-
 const generateBingo = require("./generators/bingo")
+const generatePDF = require("./services/pdfService")
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.post("/generate-bingo", (req, res) => {
-  const html = generateBingo(req.body)
-
-  res.send(html)
+app.post("/generate-bingo", async (req, res) => {
+    const html = generateBingo(req.body)
+    const pdf = await generatePDF(html)
+    res.set({
+        "Content-Type": "application/pdf",
+        "Content-Disposition": "attachment; filename=bingo.pdf"
+    })
+    res.send(pdf)
 })
 
 app.listen(3000, () => {
---
+++ backend/services/pdfService.js
@@ -0,0 +1,21 @@
+const puppeteer = require("puppeteer")
+
+async function generatePDF(html) {
+
+  const browser = await puppeteer.launch()
+
+  const page = await browser.newPage()
+
+  await page.setContent(html, { waitUntil: "networkidle0" })
+
+  const pdf = await page.pdf({
+    format: "A4",
+    printBackground: true
+  })
+
+  await browser.close()
+
+  return pdf
+}
+
+module.exports = generatePDF
