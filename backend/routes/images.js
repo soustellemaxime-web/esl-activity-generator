8deb1005e8fdcca1619df09f25885f22299b61eb
Maxime Soustelle <soustelle.maxime@gmail.com>
2026-03-19 10:30:49 +0700

    images cache

--- backend/routes/images.js
+++ backend/routes/images.js
@@ -1,33 +1,18 @@
 const express = require("express");
-require("dotenv").config();
-
 const router = express.Router();
 
-const cache = {};
+const { getImages } = require("../services/imageService");
 
 router.get("/", async (req, res) => {
-  const word = req.query.word?.toLowerCase();
+  const word = req.query.word;
 
   if (!word) {
     return res.status(400).json({ error: "Missing word" });
   }
 
-  if (cache[word]) {
-    return res.json({ source: "cache", images: cache[word] });
-  }
-
   try {
-    const url = `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${encodeURIComponent(word)}&image_type=photo&per_page=5`;
-
-    const response = await fetch(url);
-    const data = await response.json();
-
-    const images = data.hits.map(img => img.webformatURL);
-
-    cache[word] = images;
-
-    res.json({ source: "api", images });
-
+    const result = await getImages(word);
+    res.json(result);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Image fetch failed" });
--- backend/services/imageService.js
+++ backend/services/imageService.js
@@ -1,16 +1,37 @@
+require("dotenv").config();
+
 const API_KEY = process.env.PIXABAY_API_KEY
+const cache = {};
+const CACHE_DURATION = 1000 * 60 * 60 * 24; // 24h
 
-async function getImage(word) {
+async function getImages(word) {
+  const key = word.toLowerCase();
 
-  const url =
-    `https://pixabay.com/api/?key=${API_KEY}&q=${word}&image_type=vector&per_page=3`
+  // 1. Check cache
+  if (cache[key]) {
+    const { data, timestamp } = cache[key];
 
-  const res = await fetch(url)
-  const data = await res.json()
+    if (Date.now() - timestamp < CACHE_DURATION) {
+      return { source: "cache", images: data };
+    }
+  }
 
-  if (data.hits.length === 0) return null
+  // 2. Fetch from Pixabay
+  const query = `${key} cartoon`; //make it kid-friendly //TODO: let the user choose style
+  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&per_page=3`;
 
-  return data.hits[0].webformatURL
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const image = data.hits[0]?.webformatURL || null;
+
+  // 3. Save to cache
+  cache[key] = {
+    data: image,
+    timestamp: Date.now()
+  };
+
+  return { source: "api", image };
 }
 
-module.exports = getImage
+module.exports = { getImages };
