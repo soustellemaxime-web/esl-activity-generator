90595e1e42781c16f90672fd5579ed7afacc8cef
Maxime Soustelle <soustelle.maxime@gmail.com>
2026-04-05 15:23:31 +0700

    .

--- backend/services/pdfService.js
+++ backend/services/pdfService.js
@@ -4,7 +4,6 @@ async function generatePDF(html) {
 
   const browser = await puppeteer.launch({
     headless: "new",
-    executablePath: process.env.CHROME_EXECUTABLE_PATH || puppeteer.executablePath(),
     args: ["--no-sandbox", "--disable-setuid-sandbox"]
   })
 
