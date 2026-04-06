85b109c5585f9f14993f62512e2d8181826942b7
Maxime Soustelle <soustelle.maxime@gmail.com>
2026-04-06 15:45:12 +0700

    Small adjustments

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,9 +1,7 @@
 async function initHome() {
   const { data: { user } } = await supabaseClient.auth.getUser();
-
   if (user) {
     document.getElementById("dashboardBtn").classList.remove("hidden");
-    document.getElementById("loginHint").classList.add("hidden");
   }
 }
 
