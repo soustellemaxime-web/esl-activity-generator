04758db3f6ed67cb0d680237e612ac5e8c94ff13
Maxime Soustelle <soustelle.maxime@gmail.com>
2026-04-06 15:18:59 +0700

    Added dashboard button to main page

---
+++ frontend/js/index.js
@@ -0,0 +1,14 @@
+async function initHome() {
+  const { data: { user } } = await supabaseClient.auth.getUser();
+
+  if (user) {
+    document.getElementById("dashboardSection").classList.remove("hidden");
+    document.getElementById("loginHint").classList.add("hidden");
+  }
+}
+
+function goToDashboard() {
+  window.location.href = "dashboard.html";
+}
+
+document.addEventListener("DOMContentLoaded", initHome);
